02801d2fe8e3b8792f68ae707d6f8f6baab97f48
Stuart Wilkins <swilkins@bnl.gov>
2014-11-30 06:21:16 -0500

    Added message to get_text_from_editor

--- pyOlog/utils.py
+++ pyOlog/utils.py
@@ -4,6 +4,14 @@ import tempfile
 
 from OlogDataTypes import Attachment
 
+text_message = '''
+#
+# Please enter the log message using the editor. Lines beginning
+# with '#' will be ignored, and an empty message aborts the log
+# message from being logged.
+#
+'''
+
 def get_screenshot(root = False, itype = 'png'):
   """Open ImageMagick and get screngrab as png."""
   if root:
@@ -17,10 +25,41 @@ def get_screenshot(root = False, itype = 'png'):
 
   return Attachment(img, 'screenshot.' + itype)
 
-def get_text_from_editor():
+def get_text_from_editor(additional_text = None):
   """Open text editor and return text"""
   with tempfile.NamedTemporaryFile(suffix='.tmp') as f:
+    # Write out the file and flush
+    message = text_message
+    if not additional_text: 
+      message += ''
+    else:
+      message += additional_text
+    f.write(message)
+    f.flush()
+
+    # Now open editor to edit file
     editor = os.environ.get('EDITOR', 'vim')
     subprocess.call([editor, f.name])
+
+    # Read file back in
+    f.seek(0)
     text = f.read()
+
+    # Strip off any lines that start with whitespace and a '#'
+    lines = [n for n in text.splitlines() if not n.lstrip().startswith('#')]
+    text = ''.join(lines)
   return text
+
+def get_pyplot_fig(self, *args, **kwargs):
+  """Save a matplotlib figure as an Attachment"""
+  import matplotlib.pyplot as plt
+  import StringIO
+
+  imgdata = StringIO.StringIO()
+  plt.savefig(imgdata, format = 'png', **kwargs)
+  imgdata.seek(0)
+
+  a = Attachment(imgdata, 'plot.png')
+
+  return a
+
